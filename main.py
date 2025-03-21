b1616f436ad4e397f9b35b8a709588d1120aebe7
palver11 <monkbezveri@gmail.com>
2025-03-21 23:16:53 +0700

    second commit

--- main.py
+++ main.py
@@ -10,17 +10,18 @@ from tkcalendar import Calendar
 BLACK = "#0a0a0a"
 WHITE = "#cccccc"
 RED   = "#f08080"
+DGRAY = "#808080"
 GRAY  = "#b4b4b4"
 LGRAY = "#e3e3e3"
 
 
 class Application(tk.Frame):
     def __init__(self, master=None):
-        tk.Frame.__init__(self, master, padx=5, pady=5, bg=GRAY)
+        tk.Frame.__init__(self, master, bg=DGRAY)
         
         self.grid()
         self.createWidgets()
-        # self.resizable(self, width=0, height=0) # TODO: figure out how to disable resizing
+        
 
     def createWidgets(self):
         
@@ -28,13 +29,14 @@ class Application(tk.Frame):
         # SET WIDGETS
         # TODO: don't copypaste more timers, make it modulus with a function
         # Timer 1
-        txt_1 = tk.Text()
-        ent_1 = tk.Entry(self, bg=LGRAY, fg=BLACK, width=40)
-        btn_1 = tk.Button(self, text='○', command=self.calendar_popup, bg=RED, fg=BLACK, width=2, cursor="hand2")
-        calen = Calendar(self, selectmode="day")
-        lbl_1 = tk.Label(self, text="0|00:00", fg=BLACK, width=20)
-        sep_1 = ttk.Separator(self, orient=HORIZONTAL)
+        frame = Frame(self, bg=GRAY)
+        ent_1 = tk.Entry(frame, bg=LGRAY, fg=BLACK, width=40, cursor="xterm")
+        btn_1 = tk.Button(frame, text='○', command=self.calendar_popup, bg=RED, fg=BLACK, width=2, cursor="hand2")
+        calen = Calendar(frame, selectmode="day")
+        lbl_1 = tk.Label(frame, text="0|00:00", bg=GRAY, fg=BLACK, width=20)
+        sep_1 = ttk.Separator(frame, orient=HORIZONTAL)
 
+        frame.grid(row=0, column=0, ipadx=10, ipady=10)
         ent_1.grid(row=0, column=0, padx=5, pady=10, columnspan=2)
         btn_1.grid(row=0, column=3, padx=5, pady=10)
         calen.grid(row=1, column=0, padx=5, pady=10)
@@ -46,13 +48,16 @@ class Application(tk.Frame):
     def calendar_popup(self) -> None:
         popup_win = Toplevel(self)
         popup_win.title("Calendar")
-        popup_win.geometry(f"100x100+{self.winfo_pointerx() - 100}+{self.winfo_pointery() - 100}")
+        popup_win.geometry(f"260x200+{self.winfo_pointerx() - 100}+{self.winfo_pointery() - 100}")
         popup_win.resizable(width=0, height=0)
         calen = Calendar(popup_win, selectmode="day")
         calen.grid(row=1, column=0, padx=5, pady=10)
 
  
+root = Tk()
+# root.geometry("480x900+200+200")
+root.resizable(False, False)
+root.title('Deadliner pre-alpha')
 app = Application()
-app.master.title('Deadliner')
-# app.config(padx = 5, pady = 5)
+app.config(padx=5, pady=5, bg=DGRAY)
 app.mainloop()
