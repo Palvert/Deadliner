03fd8471f2dca6f6246875dd323c33dd5202f08b
palver11 <monkbezveri@gmail.com>
2025-03-22 23:59:30 +0700

    style: update fonts and favicon

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ RED   = "#f08080"
 DGRAY = "#808080"
 GRAY  = "#b4b4b4"
 LGRAY = "#e3e3e3"
+GREEN = "#94d692"
 TESTCLR = "#ff9900"
 
 win_size: str = "" #Do not reasign directly
@@ -33,11 +34,11 @@ class Application(tk.Frame):
         # TODO: don't copypaste more timers, make it modulus with a function
         # Timer 1
         frame = Frame(self, bg=GRAY)
-        ent_timer = tk.Entry(frame, bg=LGRAY, fg=BLACK, width=40, cursor="xterm")
-        btn_set          = tk.Button(frame, text='Set', command=self.calendar_popup, bg=LGRAY, fg=BLACK, cursor="hand2")
-        #NOTE: potential problems with label's width (may move the buttons), also, need to think about the font
-        lbl_time_left    = tk.Label(frame, text="0000 | 00:00", bg=GRAY, fg=BLACK, font="bebas") 
-        btn_reset        = tk.Button(frame, text='○', command=self.reset_timer, bg=RED, fg=BLACK, width=2, cursor="hand2")
+        ent_timer = tk.Entry(frame, bg=LGRAY, fg=BLACK, width=20, cursor="xterm", font=("Arial 12"))
+        btn_set          = tk.Button(frame, text='<', command=self.calendar_popup, bg=GREEN, fg=BLACK, width=2, cursor="hand2", font=("Arial 9 bold"))
+        #NOTE: potential problems with label's width (may move the buttons)
+        lbl_time_left    = tk.Label(frame, text="0000 | 00:00", bg=GRAY, fg=BLACK, font="Arial 12 bold") 
+        btn_reset        = tk.Button(frame, text='>', command=self.reset_timer, bg=RED, fg=BLACK, width=2, cursor="hand2", font=("Arial 9 bold"))
         separator        = ttk.Separator(frame, orient=HORIZONTAL)
 
         # get the size of the frame to set the apropriate size of the window
@@ -69,6 +70,7 @@ class Application(tk.Frame):
 root = Tk()
 root.resizable(False, False)
 root.title('Deadliner pre-alpha')
+root.iconbitmap("deadliner.ico")
 app = Application()
 root.geometry(win_size.join("+500+500"))
 app.config(padx=5, pady=5, bg=DGRAY)
