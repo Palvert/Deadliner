8564d9eac383c9d4ee13eac721caf9a48fe2c9d5
palver11 <monkbezveri@gmail.com>
2025-03-23 21:00:13 +0700

    wip: replace calendar popup

--- main.py
+++ main.py
@@ -39,21 +39,19 @@ class Application(tk.Frame):
         
         # SET WIDGETS
         # Timer 1
-        frame = ttk.Frame(self)#, background=GRAY)
-        ent_timer = ttk.Entry(frame, bg=GRAY, fg=BLACK, width=20, cursor="xterm", font=(font_1 + font_1_size), relief="solid")
-        btn_reset     = ttk.Button(frame, 
+        frame = tk.Frame(self, background=GRAY)
+        ent_timer = tk.Entry(frame, bg=GRAY, fg=BLACK, width=20, cursor="xterm", font=(font_1 + font_1_size), relief="solid")
+        btn_reset     = tk.Button(frame, 
                                   text='ク', command=self.reset_timer, width=2,
                                   bg=RED, fg=BLACK, cursor="hand2", font=(font_2 + font_1_size + "bold"))
-        progress_bar = ttk.Progressbar(frame)
-        date_ent = ttk.DateEntry(frame, mindate=datetime.date.today(), 
+        date_ent = tkc.DateEntry(frame, mindate=datetime.date.today(), 
                                 date_pattern="dd.mm.yyyy", font=(font_1 + font_1_size))
-        lbl_time_left = ttk.Label(frame, text="0000 | 00:00", bg=GRAY, fg=BLACK, font=(font_1 + font_1_size + "bold")) 
+        lbl_time_left = tk.Label(frame, text="0000 | 00:00", bg=GRAY, fg=BLACK, font=(font_1 + font_1_size + "bold")) 
 
         # place the widgets
         frame.grid(row=0, column=0, ipadx=1)
         ent_timer.grid(row=0, column=0, padx=pdx, pady=pdy, columnspan=2)
         btn_reset.grid(row=0, column=2, padx=pdx, pady=pdy)
-        progress_bar.grid(row=1, column=0, columnspan=3)
         date_ent.grid(row=2, column=0, padx=pdx)
         lbl_time_left.grid(row=2, column=1, padx=pdx, pady=pdy, columnspan=2)
 
