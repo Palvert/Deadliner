c27f8d5e48a52b518fcc3a0ee765e604c43a6db8
palvert <>
2025-04-01 22:15:04 +0700

    feat: add load save to file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-# TODO: make the time entry fields only accept numbers
-# TODO: on start of the program timers appear on by one - it's ugly, need to fix somehow
+# TODO: on start of the program timers appear on by one - it's ugly, I need to fix somehow
+# TODO: when data will be loaded from the file, timers, that has a character which is used as delimiter will break the program
+#       make validation for a timer title entry to make the character invalid
+# TODO: I close file each time. Do I even need it?
+# TODO: Understand the variable tracking
 
 import tkinter as tk
 from tkinter import ttk
@@ -7,9 +10,13 @@ from tkinter import ttk
 import tkcalendar as tkc
 # from tkcalendar import Calendar, DateEntry
 import re
-import datetime
+import datetime as dt
+from datetime import datetime
 import sys
 
+#--------------------------------------------------
+# VARIABLES AND CONSTANTS
+#--------------------------------------------------
 
 #Color palette
 BLACK = "#0a0a0a"
@@ -20,6 +27,9 @@ GRAY  = "#b4b4b4"
 LGRAY = "#e3e3e3"
 GREEN = "#94d692"
 TESTCLR = "#ff9900" # for debugging
+
+FILE_PATH = "timers.dat"
+DFILE_DELIM = '-'
 
 # Style
 font_1, font_1_size = "Arial\ Nova", " 12 "
@@ -32,11 +42,71 @@ date_selected = ""
 timers_quantity = 0
 timers = []
 
+# Read CLI user arugment
 timers_to_add = 1
 if len(sys.argv) > 1:
     timers_to_add = int(sys.argv[1])
 
+loaded_data: list = []
 
+#--------------------------------------------------
+# FUNCTIONS
+#--------------------------------------------------
+def handle_events(e):
+    if e.keysym == "Escape":  # NOTE:SHOULD BE COMMENTED OUT IN THE RELEASE VERSION!
+        print("exiting...")
+        sys.exit()
+
+    if e.keysym=="F1": 
+        save_data_file()
+
+
+def load_data_file():
+    global loaded_data
+    data_file = open(FILE_PATH, "rt")
+
+    # file safe-check
+    if (data_file): pass
+    else: data_file = open(FILE_PATH, "xt")
+
+    # load the file as 2D array(list)
+    for line in data_file:
+        loaded_data.append(line.split(DFILE_DELIM))
+
+    data_file.close()
+
+    # Set the loaded data into the fields
+    for i, line in enumerate(loaded_data):
+        timers[i].widgets["title"].insert(0, line[0])
+        timers[i].widgets["date"].set_date(datetime.strptime(f"{line[3]}-{line[2]}-{line[1]}", "%d-%m-%Y"))
+        timers[i].widgets["time_hrs"].insert(0, line[4])
+        timers[i].widgets["time_min"].insert(0, line[5].strip()) # strip removing \n
+
+
+def calculate_deadline():
+    pass
+
+
+def save_data_file():
+    data_to_save: str = ""
+
+    # Get data from timers
+    for n in range(len(timers)):
+        timer_data: str = ( timers[n].widgets["title"].get()
+            + DFILE_DELIM + str(timers[n].widgets["date"].get_date())
+            + DFILE_DELIM + timers[n].widgets["time_hrs"].get()
+            + DFILE_DELIM + timers[n].widgets["time_min"].get() )
+        data_to_save.join(timer_data + "\n")
+
+    # Save the aquired data to the file
+    data_file = open(FILE_PATH, "wt")
+    data_file.write(data_to_save)
+    data_file.close()
+
+
+#--------------------------------------------------
+# CLASSES
+#--------------------------------------------------
 class Application(tk.Frame):
 
     def __init__(self, master=None):
@@ -48,11 +118,13 @@ class Application(tk.Frame):
         for n in range(timers_to_add):
             timers.append(Timer(self))
             timers[n].grid()
+
             # Add an improvised separator
-            # frame_x = int(re.split('x', self.winfo_geometry())[0])
-            if timers_quantity != timers_to_add: # prevents addign a separator under the bottom timer
+            if timers_quantity != timers_to_add: # if not the bottom timer
                 separator = tk.Frame(self, bg=DGRAY, height=4, width=200)
                 separator.grid()
+
+        load_data_file()
  
 
 class Timer(tk.Frame):
@@ -62,80 +134,101 @@ class Timer(tk.Frame):
 
         global timers_quantity        
         global timers
-        self.widgets = [
-            # Title entry      # 0
-            # Reset button     # 1
-            # Date entry       # 2
-            # Hours entry      # 3
-            # : label          # 4
-            # Minutes entry    # 5
-            # Time left label  # 6
-            # Separator(frame) # 7
-        ]
 
-            
-        self.widgets.append(tk.Entry(self, 
-                        bg=LGRAY, fg=BLACK, width=25, cursor="xterm", font=(font_1 + font_1_size), relief="flat"))
-        self.widgets.append(tk.Button(self, 
-                        text='ク', command=self.reset_timer, width=2,
-                        bg=RED, fg=BLACK, cursor="hand2", font=(font_2 + font_1_size + "bold")))
-        self.widgets.append(tkc.DateEntry(self, 
-                        mindate=datetime.date.today(), date_pattern="dd.mm.yyyy", font=(font_1 + font_1_size)))
-        self.widgets.append(tk.Entry(self,
-                        bg="white", fg=BLACK, width=2, cursor="xterm", font=(font_1 + font_1_size)))
-        self.widgets.append(tk.Label(self,
-                        text=":", bg=GRAY, fg=BLACK, font=(font_1 + font_1_size)))
-        self.widgets.append(tk.Entry(self,
-                        bg="white", fg=BLACK, width=2, cursor="xterm", font=(font_1 + font_1_size)))
-        self.widgets.append(tk.Label(self, 
-                        text="0000 | 00:00", bg=GRAY, fg=BLACK, font=(font_1 + font_1_size + "bold")))
-        # Add an improvised separator
-        # frame_x = int(re.split('x', self.winfo_geometry())[0])
-        # self.widgets.append(tk.Frame(self, bg=DGRAY, height=3, width=290))
-        
 
-        # place the widgets
-        self.widgets[0].grid(row=0, column=0, padx=pdx+4, pady=pdy, columnspan=6, sticky='w')
-        self.widgets[1].grid(row=0, column=7, padx=pdx+6, pady=pdy, columnspan=3)
-        self.widgets[2].grid(row=1, column=0, padx=pdx)
-        self.widgets[3].grid(row=1, column=1)
-        self.widgets[4].grid(row=1, column=2)
-        self.widgets[5].grid(row=1, column=3)
-        self.widgets[6].grid(row=1, column=4, padx=pdx, pady=pdy, columnspan=6)
-        # if timers_quantity + 1 != timers_to_add: # prevents addign a separator under the bottom timer
-        #     self.widgets[7].grid(row=2, columnspan = 8, padx=5, pady=3)
+        # TIME(CLOCK) ENTRY INPUT VALIDATION
+        def validate_input_hrs(new_text):
+            if (not new_text or new_text.isdigit() and len(new_text) <= 2 
+                and 0 <= int(new_text) and int(new_text) <= 23):
+                return True
+            else: 
+                on_invalid_hrs()
+                return False
 
-        # get the size of the frame to set the apropriate size of the window
-        # win_size = re.split(r"[\+\-]", frame.winfo_geometry())[0]
+        def validate_input_min(new_text):
+            if (not new_text or new_text.isdigit() and len(new_text) <= 2 
+                and 0 <= int(new_text) and int(new_text) <= 59):
+                return True
+            else: 
+                on_invalid_min()
+                return False
+
+        validate_cmd_hrs = root.register(validate_input_hrs) # Register the validation command
+        validate_cmd_min = root.register(validate_input_min) # Register the validation command
+
+        def on_invalid_hrs():
+            self.widgets["time_hrs"].config(bg=RED)
+            root.after(1000, lambda: self.widgets["time_hrs"].config(bg="white"))
+
+        def on_invalid_min():
+            self.widgets["time_min"].config(bg=RED)
+            root.after(1000, lambda: self.widgets["time_min"].config(bg="white"))
+
+
+        # CREATE WIDGETS
+        self.widgets = {
+            # ----------------------------------------------------------------------
+            "title":        tk.Entry(self, 
+                            bg=LGRAY, fg=BLACK, width=25, cursor="xterm", font=(font_1 + font_1_size), relief="flat"),
+            # ----------------------------------------------------------------------
+            "btn_reset":    tk.Button(self, 
+                            text='ク', command=self.reset_timer, width=2,
+                            bg=RED, fg=BLACK, cursor="hand2", font=(font_2 + font_1_size + "bold")) ,
+            # ----------------------------------------------------------------------
+            "date":         tkc.DateEntry(self, 
+                            mindate=dt.date.today(), date_pattern="dd.mm.yyyy", font=(font_1 + font_1_size)) ,
+            # ----------------------------------------------------------------------
+            "time_hrs":     tk.Entry(self,
+                            bg="white", fg=BLACK, width=2, cursor="xterm", font=(font_1 + font_1_size),
+                            validate="key",  # Validate on each key press
+                            validatecommand=(validate_cmd_hrs, '%P')), # %P = new text
+            # ----------------------------------------------------------------------
+            "time_colon":   tk.Label(self,
+                            text=":", bg=GRAY, fg=BLACK, font=(font_1 + font_1_size)) ,
+            # ----------------------------------------------------------------------
+            "time_min":     tk.Entry(self,
+                            bg="white", fg=BLACK, width=2, cursor="xterm", font=(font_1 + font_1_size),
+                            validate="key",  # Validate on each key press
+                            validatecommand=(validate_cmd_min, '%P')),  # %P = new text
+            # ----------------------------------------------------------------------
+            "time_tracker": tk.Label(self, 
+                            text="0000 | 00:00", bg=GRAY, fg=BLACK, font=(font_1 + font_1_size + "bold")) ,
+            # ----------------------------------------------------------------------
+        }
+
+
+        # PLACING THE WIDGETS
+        self.widgets["title"].grid(row=0, column=0, padx=pdx+4, pady=pdy, columnspan=6, sticky='w')
+        self.widgets["btn_reset"].grid(row=0, column=7, padx=pdx+6, pady=pdy, columnspan=3)
+        self.widgets["date"].grid(row=1, column=0, padx=pdx)
+        self.widgets["time_hrs"].grid(row=1, column=1)
+        self.widgets["time_colon"].grid(row=1, column=2)
+        self.widgets["time_min"].grid(row=1, column=3)
+        self.widgets["time_tracker"].grid(row=1, column=4, padx=pdx, pady=pdy, columnspan=6)
 
         timers_quantity += 1
 
     
     def reset_timer(self) -> None:
-        # print("Reset")
-        self.widgets[0].delete(0, tk.END)
-        # self.widgets[1]
-        self.widgets[2].set_date(datetime.date.today())
-        self.widgets[3].delete(0, tk.END)
-        # self.widgets[4]
-        self.widgets[5].delete(0, tk.END)
-        # self.widgets[6]
-        # self.widgets[7]
+        self.widgets["title"].delete(0, tk.END)
+        self.widgets["date"].set_date(dt.date.today())
+        self.widgets["time_hrs"].delete(0, tk.END)
+        self.widgets["time_min"].delete(0, tk.END)
 
 
-def terminate_program(e):
-    if e.keysym == "Escape":
-        sys.exit()
-
+#--------------------------------------------------
+# MAIN PROCESS
+#--------------------------------------------------
 
 root = tk.Tk()
-# root.resizable(False, False)
+root.resizable(False, False)
 root.title('Deadliner pre-alpha')
 root.iconbitmap("deadliner.ico")
 app = Application()
 root.geometry(win_size.join("+500+500"))
 app.config(padx=5, pady=5, bg=DGRAY)
+# TODO: saving data every 1 minute to the file
 # key events
-root.bind("<KeyRelease>", terminate_program) # NOTE:SHOULD BE COMMENTED OUT IN THE RELEASE VERSION!
+root.bind("<KeyRelease>", handle_events)
 
 app.mainloop()
